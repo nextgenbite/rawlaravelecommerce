eee99ffbc0b624217126eaeb97ded2423fb6de78
nextgenbite <iliusrahman@gmail.com>
2022-04-14 16:44:33 +0600

    frontend added

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url('postgres://gwqxohszitnjbl:ba00e6361a6980a29264aa5465d68f27071c219ff7cce3a8061fa58989823eef@ec2-34-192-210-139.compute-1.amazonaws.com:5432/d5gft24shjo1j0');
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,15 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
